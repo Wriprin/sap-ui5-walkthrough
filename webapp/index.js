531495489e32262e1df7c33bdbc78e0fdf017cf1
Wriprin <wriprin@gmail.com>
2022-11-08 17:52:24 +0800

    Step 4: XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-	"sap/m/Text"
+// 5. 引入 SAP XMLView（由 SAPUI5 构建），用于唯一指定资源
+// 该 Name 有 sap.ui.demo.walkthrough.view.* 做前缀，用于唯一指定，参见 13 行
+	"sap/ui/core/mvc/XMLView"
 ], function (
-	Text) {
+	XMLView) {
 	"use strict";
 
-	// 2. 将 Text 对象 指定到 id 为 "content" 的 DOM 节点对象 -> Body
-	new Text({
-		text: "Hello, World!"
-	}).placeAt(content);
+	// 6. 将 App.view.xml 实例化 匿名对象
+	// 7. 调用 then() 逻辑处理
+	// 7.1 （这里做了将 匿名对象 oView（也就是 第六步实例化的对象）绑定到 id 为 "content" 的 DOM 节点对象身上）
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) { 
+		oView.placeAt("content");
+	 });
 });
