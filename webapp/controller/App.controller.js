899b3bfc3362611282b56dc859cd89cf10861f74
Wriprin <wriprin@gmail.com>
2022-11-09 09:35:48 +0800

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,20 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+    // 1. 引入 MessageToast 控件
+    // 1.1 主要想表明 该部分 引用可以异步加载，提升性能
+    //  The browser can decide when and how the resources are loaded prior to code execution.
+    "sap/m/MessageToast"
 ], function(
-	Controller
+	Controller,
+    // 2. 对象入参
+	MessageToast
 ) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        // 3. 完成 onShowHello 方法 具体逻辑处理
         onShowHello : function () {
-            // 4. Button 的 Press 事件触发后，弹窗显示 Hello, World.
-            alert("Hello, World.");
+            // 3. 调用 show() 方法 指定 iMessage 进行消息提示
+            MessageToast.show("Hello, World.")
         }
 	});
 });
