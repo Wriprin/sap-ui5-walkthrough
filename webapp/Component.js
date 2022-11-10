f4d530e9867263f5f2986d92ceaf95f4a4513b3b
Wriprin <wriprin@gmail.com>
2022-11-10 16:17:01 +0800

    Step 10: Descriptor for Applications

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,34 +1,20 @@
-/**
- * 1. 首先创建了 Component.js 文件：目前包含了两个部分：
- *      - metadata 元数据
- *          - 规定了 rootView，初始 View 页面是谁
- *      - init 继承自派生类的构造方法，必须重写
- *          - 当 Component 加载时，调用 init 方法 初始化（定义初始加载时需要的逻辑功能）
- * PS: 现在我们可以通过 Component 来管理显示 app View（之前是直接加载 index.js 中的 rootView）
- */
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    // "sap/ui/model/resource/ResourceModel"
 ], function (UIComponent,
-    JSONModel,
-    ResourceModel) {
+    JSONModel) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-                "id": "app"
-            }
+            // 7. Refer the manifest.json File 加载配置
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
         },
         init: function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
 
-            // 2. 配置 dataModel
             var oData = {
                 recipient: {
                     name: "World"
@@ -38,12 +24,14 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // 3. 配置 i18nModel
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-
-            this.setModel(i18nModel, "i18n");
+            /**
+             * We can also remove the dependency to sap/ui/model/resource/ResourceModel and the corresponding formal parameter ResourceModel
+             * Because we will not use this inside our anonymous callback function.
+             * 
+             * 8. 我们已经在 manifest.json 中配置了 i18n 的 Model（可参见 manifest.json -「34 - 45」）
+             * 8.1 所以已经可以直接在 *.controller.js 中获取已配置的 ResourceModel（可参见 App.controller.js -「12」）
+             * 8.2 因此我们可以删除掉 ResourceModel 的引用，并且再也不需要去创建 ResourceModel 的匿名对象了
+             */
         }
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,26 +8,7 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        /** 官方 Wiki
-         * In the init function we instantiate our data model and the i18n model like we did before in the app controller.
-         * Be aware that the models are directly set on the component and not on the root view of the component.
-         * However, as nested controls automatically inherit the models from their parent controls,
-         * the models will be available on the view as well.
-         */
-
-        /** 官方 Wiki 拆解分析
-         * Model 是绑定到了 Component 上，而不是 Component 对应的 rootView 上
-         * 虽然，i18n 和 data Model 是绑定到了 Component 上
-         * 但是 嵌套控件 会自动继承他们的 父类控件的 models
-         * 上面这句话翻译一下就是：我们的 Controller 对应的 View 可以直接拿到 Component 中构建好的 Models 使用
-         * 同理，Controller 中 this.getView().getModel() 就可以有效的拿到对应的 Model
-         * 因此 代码 31 行，32 行就是有效的
-         */
-
-        // 这样一来，我们将初始加载 Init 交给了 Component 来完成，Controller 则专注于 对应 View 的逻辑处理即可
-        
         onShowHello : function () {
-            // 4. 获取 i18nModel 和 DataModel 数据，占位符“拼接”，并弹窗显示 
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
             var sRecipient = this.getView().getModel().getProperty("/recipient/name");
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
