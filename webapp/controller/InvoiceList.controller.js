02f862bdda79c8b689bf62f22c9f7133a3ba783d
Wriprin <wriprin@gmail.com>
2022-11-29 13:30:05 +0800

    Step 22: Custom Formatters

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
+    // 6. 引入 formatter
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], function (Controller, JSONModel, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-
+        // 7. The controller simply stores the loaded formatter functions in the local property formatter to be able to access them in the view.
+        formatter: formatter,
 		onInit : function () {
             // 创建 Currency JSONMOdel，并绑定到视图
 			var oViewModel = new JSONModel({
