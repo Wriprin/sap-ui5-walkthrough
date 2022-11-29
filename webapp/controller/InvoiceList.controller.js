9cf2f4cf9999ba9e64897946afd961c96ddf2427
Wriprin <wriprin@gmail.com>
2022-11-29 15:02:52 +0800

    Step 23: Filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,13 +1,14 @@
 sap.ui.define([
-    // 6. 引入 formatter
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-    "../model/formatter"
-], function (Controller, JSONModel, formatter) {
+    "../model/formatter",
+    // 3. 引入相关依赖
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-        // 7. The controller simply stores the loaded formatter functions in the local property formatter to be able to access them in the view.
         formatter: formatter,
 		onInit : function () {
             // 创建 Currency JSONMOdel，并绑定到视图
@@ -15,7 +16,32 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
-		}
+		},
 
-	});
+        /**
+         * Invoice List 的 Search Field 搜索功能
+         * @param {*} oEvent 
+         * oEvent - Event handlers always receive an event argument that can be used to access the parameters that the event provides
+         * In our case the search field defines a parameter query that we access by calling getParameter(“query”) on the oEvent parameter.
+         */
+        onFilterInvoices: function (oEvent) {
+            // 获取用户键入内容
+            var sQuery = oEvent.getParameter("query");
+
+            // 配置 Filter
+            var aFilter = [];
+            // if the query is empty, we filter the binding with an empty array. This makes sure that we see all list elements again
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            // 获取 items 行项数据，并调用 Filter 搜索
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+
+            // PS: The filter operator FilterOperator.Contains is「not」case-sensitive.
+            oBinding.filter(aFilter);
+        }
+
+    });
 });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -3,23 +3,13 @@ sap.ui.define([], function () {
 	return {
 
         /**
-         * 1. 在 Step 20 我们是学习了 简单的 运算/判断表达式 进行简单的逻辑处理
-         * 2. 如果需要进行一些复杂的逻辑处理，那么就需要单独分离出来 将所有的可复用逻辑整合到一起 -> formatter.js
-         *  - formatter.js 和 
-         * 3. 如下是对 invoice JSON 对象的 status 数据进行逻辑判断处理并赋值
-         * 4. 那么为什么不写在对应的 Controller 下呢？
-         *  - 4.1 这不是事件处理（次要原因）
-         *  - ※ 4.2 formatter.js 专门用于处理 data model 的相关格式化逻辑（主要原因）
-         *  - 如下为官方定义：
-         *      - Formatters are working on data properties and format them for display on the UI.
-         * @param {*} sStatus 状态 Code
+         * 判断 invoice 对象 的 status 属性值，并获取对应的 i18n 文本内容
+         * @param {*} sStatus 
          * @returns 
          */
-
 		statusText: function (sStatus) {
             var resourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
-            // 5. 判断 invoice 对象 的 status 属性值，并获取对应的 i18n 文本内容
             switch (sStatus) {
                 case "A":
                     return resourceBundle.getText("invoiceStatusA");
